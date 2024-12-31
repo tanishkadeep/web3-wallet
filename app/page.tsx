5e57e48f20e7c4d2252a278672264f9e60d0be02
Tanishka <tanishkadeep09@gmail.com>
2025-01-01 03:08:18 +0530

    homepage ui

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center flex-col">
-      <div className="text-4xl font-bold mb-6 text-indigo-500">Web3 Wallet</div>
+      <div className="text-4xl font-bold mb-6">Web3 Wallet</div>
 
       <Link
         href="/mnemonics"
-        className="bg-indigo-200 text-neutral-900 px-8 py-2 rounded-md font-semibold"
+        className="bg-neutral-100 text-neutral-800 px-8 py-2 rounded-md font-semibold"
       >
         Get Started
       </Link>
